f66e05ba019b69cfa8f2f10a36348e8fda514e29
Alan Tang <atang8013@gmail.com>
2020-01-02 18:22:26 -0500

    user and movie models

---
+++ server/db/models/movie.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Movie = db.define('movie', {
+  movieId: {
+    type: Sequelize.STRING
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  releaseDate: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  summary: {
+    type: Sequelize.STRING
+  }
+})
